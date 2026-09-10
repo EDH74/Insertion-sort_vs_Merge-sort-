85a2041d2e64c13dbc7df360ac24d4f250756b09
Eduardo Araujo dos Santos <eduardo.santos@rede.ulbra.br>
2026-09-09 22:29:17 -0300

    Automatizando a execulcao do codigo e gerando list com a mediana dos dados [1000, 2000, 4000, 8000, 16000]

--- src/insertionSort.py
+++ src/insertionSort.py
@@ -1,6 +1,6 @@
 import random
 import timeit
-
+import statistics
 
 def insertion_sort(arr):
     for i in range(1, len(arr)):
@@ -11,16 +11,53 @@ def insertion_sort(arr):
             j = j - 1
         arr[j + 1] = key
 
+
+
+
+def execucao(listaExec):
+    arr = []
+    tempo = {}
+
+    for i in qntArray:
+        for e in range(i):
+            random.seed(67)
+            arr.append(random.randrange(i))
+        
+        tempo[i] = timeit.timeit(lambda: insertion_sort(arr), number=3)
+        arr.clear()
+        
+        
+    return tempo
+
+
 qntArray = [1000, 2000, 4000, 8000, 16000]
 
-arr = []
-tempo = {}
+soma = {}
 
-for i in qntArray:
-    for e in range(i):
-        arr.append(random.randrange(i))
+for i in range(4):
+    tempos = execucao(qntArray)
     
-    tempo[i] = timeit.timeit(lambda: insertion_sort(arr), number=100)
-    arr.clear()
+    if i == 0:
+        print("Primeira execucao descartada do calculo da mediana")
+        for i in tempos.keys():
+            soma[i] = []
+    
+    print(f"Insertion Sort execucao {i+1}")
+    for timers in tempos:
+        print(f"{timers} | {tempos[timers]}")
+        if i > 1:
+            soma[timers].append(tempos[timers])
+    print()
+    
+        
+print("=" *30)
 
-print(tempo)
+
+medianaList = []
+
+for i in soma:
+    medianaList.append(statistics.median(soma[i])) 
+    
+print(medianaList)
+
+
